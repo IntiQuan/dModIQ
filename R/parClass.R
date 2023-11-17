551585387f93620728d460d02dc8600359b6639d
Daniel Kaschek <daniel.kaschek@intiquan.com>
2023-11-17 16:56:39 +0100

    BUGFIX: warning in as.parframe

--- R/parClass.R
+++ R/parClass.R
@@ -129,7 +129,7 @@ as.parframe.parlist <- function(x, sort.by = "value", ...) {
   m_parframe <- cbind(m_parframe,
                       as.data.frame(t(vapply(x[m_idx], function(.x) .x$argument, x[[1]]$argument))))
   # Sort by value
-  m_parframe <- m_parframe[order(m_parframe[sort.by]),]
+  m_parframe <- m_parframe[order(m_parframe[[sort.by]]),]
   
   parframe(m_parframe, parameters = names(x[[m_idx[1]]]$argument), metanames = m_metanames)
 }
